d378689f1339221576a23661a98a0e43bd0dfad4
Murangi <nemandalalimurangi7@gmail.com>
2024-11-01 21:06:30 +0200

    Updated marble Player
    Removed a constraint that was limit

--- CyberTribe_Game/Assets/Scripts/MarblePlayer1.cs
+++ CyberTribe_Game/Assets/Scripts/MarblePlayer1.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class MarblePlayer1 : MonoBehaviour
 {
-
     private GameObject MousePointA;
     private GameObject MousePointB;
     private GameObject arrow;
     private GameObject circle;
 
-    //Current Distance Variables
+    // Current Distance Variables
     private float CurrentDistance;
-    public  float MaxDistance = 3f;
+    public float MaxDistance = 3f;
     private float SafeSpace;
     private float ShootPower;
 
     private Vector3 ShootDirection;
-    
+
     private void Awake()
     {
         MousePointA = GameObject.FindGameObjectWithTag("PointA");
@@ -30,7 +29,7 @@ public class MarblePlayer1 : MonoBehaviour
     {
         CurrentDistance = Vector3.Distance(MousePointA.transform.position, transform.position);
 
-        if(CurrentDistance <= MaxDistance)
+        if (CurrentDistance <= MaxDistance)
         {
             SafeSpace = CurrentDistance;
         }
@@ -39,37 +38,33 @@ public class MarblePlayer1 : MonoBehaviour
             SafeSpace = MaxDistance;
         }
 
-        //Calculate shot power and direction
+        // Calculate shot power and direction
         ShootPower = Mathf.Abs(SafeSpace) * 70;
         Vector3 DimensionsXY = MousePointA.transform.position - transform.position;
         float Difference = DimensionsXY.magnitude;
 
         MousePointB.transform.position = transform.position + ((DimensionsXY / Difference) * CurrentDistance * -1);
 
-        //This is a restraint so that it doesnt move outside the boundaries.
-        MousePointB.transform.position = new Vector3( MousePointB.transform.position.x, 10f ,MousePointB.transform.position.z);
+        // Restrict MousePointB to the marble’s y-position to keep it level
+        MousePointB.transform.position = new Vector3(MousePointB.transform.position.x, transform.position.y, MousePointB.transform.position.z);
 
-        
-
-        ShootDirection = Vector3.Normalize(MousePointA.transform.position - transform.position);
+        // Calculate ShootDirection only in the x-z plane
+        ShootDirection = Vector3.Normalize(new Vector3(DimensionsXY.x, 0f, DimensionsXY.z));
     }
 
     private void OnMouseUp()
-    {   
+    {
         arrow.GetComponent<Renderer>().enabled = false;
         circle.GetComponent<Renderer>().enabled = false;
-        Vector3 Push = ShootDirection * ShootPower * -1;
+
+        // Apply force in the x-z plane only, restricting y
+        Vector3 Push = new Vector3(ShootDirection.x, 0f, ShootDirection.z) * ShootPower * -1;
         GetComponent<Rigidbody>().AddForce(Push, ForceMode.Impulse);
     }
 
     private void ArrowAndCircleFunctionality()
     {
-       arrow.GetComponent<Renderer>().enabled = true;
-       circle.GetComponent<Renderer>().enabled = true;   
-
-       //Calculate the distance
-       
-
+        arrow.GetComponent<Renderer>().enabled = true;
+        circle.GetComponent<Renderer>().enabled = true;
     }
 }
-
