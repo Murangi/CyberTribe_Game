df6cf9e3e97cc4ec89d259ad01e8da00df18e949
whileNotEOThori <140858944+whileNotEOThori@users.noreply.github.com>
2024-11-28 20:22:11 +0200

    Incomplete Background Music implementation

---
+++ CyberTribe_Game/Assets/Scripts/BackgroundMusicController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the AudioSource component
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Play the music
+    public void PlayMusic()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    // Pause the music
+    public void PauseMusic()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+        }
+    }
+
+    // Stop the music
+    public void StopMusic()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    // Toggle mute/unmute
+    public void ToggleMute(bool isMuted)
+    {
+        audioSource.mute = isMuted;
+    }
+}
--- CyberTribe_Game/Assets/Scripts/TurnManager.cs
+++ CyberTribe_Game/Assets/Scripts/TurnManager.cs
@@ -194,3 +194,39 @@ public class TurnManager : MonoBehaviour
         EndTurn();
     }
 }
+/*using UnityEngine;
+
+
+public class MarbleHealth : MonoBehaviour
+{
+    public int health = 10; // Starting health points for the marble
+    private string OpponentTag;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        OpponentTag = (gameObject.GetComponent<TurnManager>().currentPlayer == 1) ? "Marble_1" : "Marble_6";
+
+        // Check if the collision is with an opponent's marble
+        if (collision.gameObject.CompareTag(OpponentTag))
+        {
+            collision.gameObject.GetComponent<MarbleHealth>().TakeDamage(1);
+            Debug.Log($"{gameObject.name} was hit! Remaining health: {health}\t\t hit by marble{collision.gameObject.name}");
+
+            // Check if health is zero or less
+            if (health <= 0)
+                DestroyMarble();
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    private void DestroyMarble()
+    {
+        // Debug.Log($"{gameObject.name} is destroyed!");
+        Destroy(gameObject);
+    }
+}
+*/
